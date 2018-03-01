7ddf7f0e4b2abc6618328982433feb7e115ff592
Raghu Betina <raghu@firstdraft.com>
2018-03-01 02:48:32 +0000

    Automatically associate comment to signed in user

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -47,8 +47,6 @@ class VotesController < ApplicationController
 
   def update
     @vote = Vote.find(params[:id])
-
-    @vote.user_id = params[:user_id]
     @vote.photo_id = params[:photo_id]
 
     save_status = @vote.save
