e459d3f5bf30160bf942b89e97a57cf6833be915
Raghu Betina <raghu@firstdraft.com>
2018-03-01 02:47:50 +0000

    Indirect association between users and photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,10 @@ class User < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :timeline,
+             :through => :follows,
+             :source => :photos
+
   has_many   :liked_photos,
              :through => :likes,
              :source => :photo
